ce9ad30d52fdf8888b24572d9d0800a011273586
Ilya Fomchenko <ilya.elfact@gmail.com>
2020-02-25 23:03:56 +0300

    install babel, setting it and import Vue for example

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/common'
+// примеры подключения библиотек
+// import 'vue' - поиск идет в начальное директории, затем в note_modules
+// import 'bootstrap/dist/css/bootstrap.min.css' - для подключения какого то небольшого участка
+// import Vue from 'vue' - подключение для непосрдственного использования в этом скрипте
+// Vue.use() - пример использования
+// window.Vue = require('vue') - "подвешивание в окне" на примере vue
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,14 @@ module.exports = {
   output: {
     filename: '[name].js', // точка выхода, файл в который собираются библиотеки, [name] в данном случае = app
     path: path.resolve(__dirname, './dist'), // путь использующий константу с параметрами
-    publicPath: './dist' // путь для dev server (?)
+    publicPath: '/dist' // путь для dev server (?)
+  },
+  module: {  // плагины
+    rules: [{  //правила
+      test: /\.js$/,  //проверка js файлов
+      loader: 'babel-loader', // плагин через который идет проверка
+      exclude: '/node_modules/' //исключения для проверки
+    }]
   },
   devServer: { //настройки плагина dev-server
     overlay: true // показ ошибок в браузере
